b1ad0b9cf9e4ba095855ba1ee9d40e8fd7e87de4
wangzhqiang <1317649253@qq.com>
2017-06-28 20:52:22 +0800

    second

--- day2/myecho1/echo.go
+++ day2/myecho1/echo.go
@@ -7,8 +7,12 @@ import (
 )
 
 var sep = flag.String("s","","separator")
+var newline = flag.Bool("n",true,"append newline")
 func main() {
    flag.Parse()
    fmt.Print(strings.Join(flag.Args(),*sep))
+   if *newline {
+   fmt.Println()
+}
     
 }
