57c439badcac3bde4dfef4b942b7f3779a4679e4
wangzhqiang <1317649253@qq.com>
2017-06-28 09:01:39 +0800

    third

--- day2/myecho/init.go
+++ day2/myecho/init.go
@@ -8,10 +8,17 @@ func main() {
 	var c float32
 	var d bool
 	var p *int
+        i := 1
+        s := "hello"
+        j,k := "yes",false
 	fmt.Printf("%v\n", a)
 	fmt.Printf("%v\n", b)
 	fmt.Printf("%v\n", c)
 	fmt.Printf("%v\n", d)
 	fmt.Printf("%v\n", p)
+	fmt.Printf("%v\n", i)
+	fmt.Printf("%v\n", s)
+	fmt.Printf("%v\n", j)
+	fmt.Printf("%v\n", k)
 
 }
