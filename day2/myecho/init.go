ae917aa0f8efdcd8f2f6b6e7a8b1582b0814f07d
wangzhqiang <1317649253@qq.com>
2017-06-28 08:53:26 +0800

    second

--- day2/myecho/init.go
+++ day2/myecho/init.go
@@ -6,10 +6,12 @@ func main() {
 	var a string
 	var b int
 	var c float32
+	var d bool
 	var p *int
 	fmt.Printf("%v\n", a)
 	fmt.Printf("%v\n", b)
 	fmt.Printf("%v\n", c)
+	fmt.Printf("%v\n", d)
 	fmt.Printf("%v\n", p)
 
 }
