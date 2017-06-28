a10434dc4d78350ab24a074ad7fa2714fbe0de5b
wangzhqiang <1317649253@qq.com>
2017-06-28 08:48:00 +0800

    first

---
+++ day2/myecho/init.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string
+	var b int
+	var c float32
+	var p *int
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%v\n", p)
+
+}
