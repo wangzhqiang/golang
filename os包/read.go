756acbb3a2a36bf2420206f41ff39a4988b5a251
wangzhqiang <1317649253@qq.com>
2018-01-11 12:29:45 +0800

    os包学习

--- os包/read.go
+++ os包/read.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"os"
+	"fmt"
+	"io"
+)
+
+func main(){
+	file,err := os.Open("./proverbs.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	p := make([]byte,4)
+
+	for {
+		n,err := file.Read(p)
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(string(p[:n]))
+	}
+}
--- os包/write.go
+++ os包/write.go
@@ -1,1 +1,35 @@
-package os包
+package main
+
+import (
+	"os"
+	"log"
+	"fmt"
+)
+
+func main(){
+	proverbs := []string{
+		"Channels orchestrate mutexes serialize\n",
+		"Cgo is not GO\n",
+		"Errors are values\n",
+		"Don't panic",
+	}
+
+	file,err := os.Create("./proverbs.txt")    //创建文件
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	for _,p := range proverbs {
+		n,err := file.Write([]byte(p))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if n != len(p) {             //判断写入是否成功
+			fmt.Println("failed to write data")
+			os.Exit(1)
+		}
+	}
+	fmt.Println("file write done")
+}
